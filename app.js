3934dfc6fa806f1c25d3f09535c9f0e24e70b7f5
Yellow Huang <yellow.huang@silkrode.com.tw>
2019-12-15 22:54:46 +0800

    msgBoard

--- app.js
+++ app.js
@@ -1,76 +1,59 @@
-const express = require("express");
-const app = express();
-const engine = require("ejs-locals");
-const bodyParser = require("body-parser");
-const admin = require("firebase-admin");
+// var createError = require('http-errors');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var flash = require("connect-flash");
+var validator = require("express-validator");
 
-const serviceAccount = require("./key/node-todolist-dd9f0-firebase-adminsdk-8lrqo-ed38e60325.json");
+var app = express();
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://node-todolist-dd9f0.firebaseio.com"
-});
-
-// 使用 firebase
-const fireData = admin.database();
-// console.log(fireData);
-
-app.engine("ejs", engine);
-app.set("views", "./views");
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-//增加靜態檔案的路徑
+
+// session
+var session = require("express-session");
 app.use(express.static("public"));
-
-// 增加 body 解析
+app.use(session({ secret: "mysupersecret", resave: true, saveUninitialized: true }));
+app.use(flash());
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(validator()); // tryy引入有問題
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-// 路由
-app.get("/", function(req, res) {
-  fireData.ref("todoList").once("value", function(snapshop) {
-    let data = snapshop.val();
-    // console.log(data);
-    res.render("index", { todoList: data });
-  });
+// routes
+var routes = require("./routes/index");
+var login = require("./routes/login");
+var messageBoard = require("./routes/messageBoard");
+var signup = require("./routes/signup");
+var user = require("./routes/user");
+app.use("/", routes);
+app.use("/login", login);
+app.use("/signup", signup);
+app.use("/user", user);
+app.use("/messageBoard", messageBoard);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-/*
- * API
- */
-// add
-app.post("/addTodo", function(req, res) {
-  let params = req.body.content;
-  const todoRef = fireData.ref("todoList").push();
-  todoRef.set({ content: params }).then(function() {
-    fireData.ref("todoList").once("value", function(snapshop) {
-      res.send({
-        success: true,
-        data: snapshop.val(),
-        msg: "資料取得成功"
-      });
-    });
-  });
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
-// delet
-app.post("/deletTodo", function(req, res) {
-  let id = req.body.removeId;
-  // console.log("id", id);
-  fireData
-    .ref("todoList")
-    .child(id)
-    .remove()
-    .then(function() {
-      fireData.ref("todoList").once("value", function(snapshop) {
-        res.send({
-          success: true,
-          data: snapshop.val(),
-          msg: "刪除成功"
-        });
-      });
-    });
-});
-
-// 監聽 port
-const port = process.env.PORT || 3000;
-app.listen(port);
+module.exports = app;
