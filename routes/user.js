d3dd96f1ac40674c1a97036ae2c58f2ee6162b38
Yellow Huang <yellow.huang@silkrode.com.tw>
2019-12-22 18:12:50 +0800

    會員頁面資訊顯示

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var firebaseDb = require('../connections/firebase_admin_connect');
-router.get('/', function (req, res) {
-    firebaseDb.ref('user/'+req.session.uid).once('value',function(snapshot){
-        res.render('user', { title: '會員專區',nickname: snapshot.val().nickname});
-    })
-    
-})
-module.exports = router; 
+var firebaseDb = require("../connections/firebase_admin_connect");
+router.get("/", function(req, res) {
+  // ref('{db 路徑}' + {fireUid}).once(撈出資料)
+  firebaseDb.ref("user/" + req.session.uid).once("value", function(snapshot) {
+    res.render("user", { title: "會員專區", nickname: snapshot.val().nickname });
+  });
+});
+module.exports = router;
