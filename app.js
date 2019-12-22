582b7108939847d590872d3f661295cda69b7087
Yellow Huang <yellow.huang@silkrode.com.tw>
2019-12-22 18:31:32 +0800

    檢查是否登入

--- app.js
+++ app.js
@@ -1,52 +1,51 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var app = express();
-var flash = require('connect-flash');
-var validator = require('express-validator');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const app = express();
+const flash = require("connect-flash");
+const validator = require("express-validator");
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 // session
-var session = require("express-session")
+const session = require("express-session");
 app.use(express.static("public"));
-app.use(session({ secret: 'mysupersecret', resave: true, saveUninitialized: true }));
+app.use(session({ secret: "mysupersecret", resave: true, saveUninitialized: true }));
 app.use(flash());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(validator());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // routes
-var routes = require('./routes/index');
-var login = require('./routes/login');
-var messageBoard = require('./routes/messageBoard');
-var signup = require('./routes/signup');
-var user = require('./routes/user');
-app.use('/', routes);
-app.use('/login', login);
-app.use('/signup', signup);
+const routes = require("./routes/index");
+const login = require("./routes/login");
+const messageBoard = require("./routes/messageBoard");
+const signup = require("./routes/signup");
+const user = require("./routes/user");
 
+app
+  .use("/", routes)
+  .use("/login", login)
+  .use("/signup", signup);
 
 // check login
-app.use(function(req,res,next){
-  if(req.session.uid){
-    return next();
-  }
-  res.redirect('/');
-})
-app.use('/user', user);
-app.use('/messageBoard', messageBoard);
-
+app
+  .use(function(req, res, next) {
+    if (req.session.uid) next();
+    res.redirect("/");
+  })
+  .use("/user", user)
+  .use("/messageBoard", messageBoard);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+app.use(function(req, res, next) {
+  let err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -54,27 +53,27 @@ app.use(function (req, res, next) {
 // error handlers
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     });
   });
 }
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: err
   });
 });
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
