848825a2405893447fad7f5407bec37c9680d0a8
Yellow Huang <yellow.huang@silkrode.com.tw>
2019-12-22 16:48:39 +0800

    new

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-router.get('/', function (req, res, next) {
-    res.render('index', {
-        title: '六角學院留言板'
-    });
-});
-/* GET home page. */
-module.exports = router;
